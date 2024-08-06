6fd17ed47323b812ad694de7d70017f3d0988db4
kavya <kavyagoutham2211@gmail.com>
2024-08-06 21:26:28 +0200

    Committing for the second time
    Login API test

--- src/test/java/com/api/tests/LoginAPITests.java
+++ src/test/java/com/api/tests/LoginAPITests.java
@@ -3,6 +3,7 @@ package com.api.tests;
 public class LoginAPITests {
 	public void loginAPItest() {
 		System.out.println("Running Login API Tests");
+		System.out.println("2nd TIME --> Running Login API Tests");
 	}
 
 }
